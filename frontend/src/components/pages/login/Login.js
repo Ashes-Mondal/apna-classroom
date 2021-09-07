02167c5e0187b0ddb9c8ef655c02bceeb54ec749
Ashes-Mondal <ashes.m2001@gmail.com>
2021-09-07 14:11:46 +0530

    login-protoype-1-design

--- frontend/src/components/pages/login/Login.js
+++ frontend/src/components/pages/login/Login.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './Login.scss'
+import img from '../../../images/login/undraw_Working_re_ddwy.svg'
 
 const Login = () => {
     return (
         <>
             <div className="login-background">
                 <div className="login-container">
-                    <div className="login-image"></div>
                     <form className="login-form">
-                        
+
                     </form>
+                    <div className="login-right">
+                        <div className="login-right-head">
+                            <h2>Welcome back to</h2>
+                            <h2>&lt; Project name &gt;</h2>
+                            <hr/>
+                            <h4>Sign in to continue to your account</h4>
+                        </div>
+                        <img src={img} alt="" className="login-image" />
+                    </div>
                 </div>
             </div>
 
