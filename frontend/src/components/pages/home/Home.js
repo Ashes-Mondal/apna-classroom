057cb2b35c4f38425cbf23833643a844a5ce5f69
subodhr258 <subodhr2001@gmail.com>
2021-10-02 18:02:00 +0530

    feat: Assignment Page

--- frontend/src/components/pages/home/Home.js
+++ frontend/src/components/pages/home/Home.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import Card from '../../common/cards/Card'
-import Upload from '../../common/file-upload/Upload'
+import React from "react";
+import Card from "../../common/cards/Card";
+import Upload from "../../common/file-upload/Upload";
 
 const Home = () => {
-    return (
-        <div className='d-flex flex-wrap'>
-            {/* <Card/>
-            <Card theme='red' />
-            <Card theme='blue' />
-            <Card theme='purple' />
-            <Card theme='yellow' />
-            <Card theme='green' />
-            <Card theme='orange' /> */}
-            <Upload/>
-        </div>
-    )
-}
+  return (
+    <div className="d-flex flex-wrap">
+      <Card />
+      <Card theme="red" />
+      <Card theme="blue" />
+      <Card theme="purple" />
+      <Card theme="yellow" />
+      <Card theme="green" />
+      <Card theme="orange" />
+      {/* <Upload/> */}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- frontend/src/routes/public.js
+++ frontend/src/routes/public.js
@@ -1,10 +1,15 @@
 //imports
-import Home from "../components/pages/home/Home"
+import Home from "../components/pages/home/Home";
+import Assignment from "../components/pages/Assignment/Assignment";
 
-const publicRoutes =  [
-    {
-        component:Home,
-        path:'/'
-    },
+const publicRoutes = [
+  {
+    component: Home,
+    path: "/",
+  },
+  {
+    component: Assignment,
+    path: "/Assignment",
+  },
 ];
 export default publicRoutes;
