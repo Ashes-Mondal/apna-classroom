6010e636636a3cd48aac2888921d52eededbb590
Aaryak Shah <aaryakdshah@gmail.com>
2021-10-15 22:10:16 +0530

    fix error in public.js

--- frontend/src/routes/public.js
+++ frontend/src/routes/public.js
@@ -1,33 +1,30 @@
 //imports
-import Home from "../components/pages/home/Home";
-import Assignment from "../components/pages/Assignment/Assignment";
-import Results from "../components/pages/results/Results";
-import Upload from "../components/common/file-upload/Upload";
+import Home from '../components/pages/home/Home'
+import Assignment from '../components/pages/Assignment/Assignment'
+import Results from '../components/pages/results/Results'
+import Upload from '../components/common/file-upload/Upload'
 import Classroom from '../components/pages/classroom/Classroom'
 
-
-export default publicRoutes
-
 const publicRoutes = [
-  {
-    component: Home,
-    path: "/",
-  },
-  {
-    component: Assignment,
-    path: "/Assignment",
-  },
-  {
-    component: Results,
-    path: '/results/:subject',
-  },
-  {
-    component: Upload,
-    path: '/upload',
-  },
-  {
-      component: Classroom,
-      path: '/testcls',
-  },
-];
-export default publicRoutes;
+    {
+        component: Home,
+        path: '/',
+    },
+    {
+        component: Assignment,
+        path: '/Assignment',
+    },
+    {
+        component: Results,
+        path: '/results/:subject',
+    },
+    {
+        component: Upload,
+        path: '/upload',
+    },
+    {
+        component: Classroom,
+        path: '/testcls',
+    },
+]
+export default publicRoutes
