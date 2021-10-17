a35e2e01932ae60a8ee4fe1a09d611643f8c009c
Ashes-Mondal <ashes.m2001@gmail.com>
2021-10-17 23:37:45 +0530

    GET request authorizations

--- backend/routes/authorization.js
+++ backend/routes/authorization.js
@@ -3,16 +3,17 @@ let publicRoutes = validRoutes.publicRoutes;
 let studentRoutes = validRoutes.studentRoutes;
 let teacherRoutes = studentRoutes.concat(validRoutes.teacherRoutes);
 let adminRoutes = teacherRoutes.concat(validRoutes.adminRoutes);
-const { accessTokenVerification } = require('../utils/jwt');
+const { accessTokenVerification } = require("../utils/jwt");
 
 //checkauthorization is a middleware function , checkauthorization is called before every route
 async function checkauthorization(req, res, next) {
     try {
-        if (publicRoutes.includes(req.url) || (req.url.includes('/file/') && req.method === 'GET')) {
+        if (publicRoutes.includes(req.url)) {
             next();
-        }
-        else {
-            const accessToken = req.cookies.login ? JSON.parse(req.cookies.login).accessToken : undefined;
+        } else {
+            const accessToken = req.cookies.login
+                ? JSON.parse(req.cookies.login).accessToken
+                : undefined;
             if (!accessToken) {
                 res.status(401).json({ data: null, error: "Unauthorized" });
                 return;
@@ -22,33 +23,47 @@ async function checkauthorization(req, res, next) {
             //==>if token was renewed
             if (jwtdata.newAccessToken) {
                 res.clearCookie("login");
-                res.cookie
-                    (
-                        "login",
-                        JSON.stringify({ accessToken: jwtdata.newAccessToken, sessionID: jwtdata.newAccessToken.sessionID }),
-                        { httpOnly: true, secure: process.env.NODE_ENV === "production" }
-                    );
+                res.cookie(
+                    "login",
+                    JSON.stringify({
+                        accessToken: jwtdata.newAccessToken,
+                        sessionID: jwtdata.newAccessToken.sessionID,
+                    }),
+                    {
+                        httpOnly: true,
+                        secure: process.env.NODE_ENV === "production",
+                    }
+                );
             }
 
             //2.Check whether the following request is valid
-            if (
-                (jwtdata.role === 'student' && studentRoutes.includes(req.url)) ||
-                (jwtdata.role === 'teacher' && teacherRoutes.includes(req.url)) ||
-                (jwtdata.role === 'admin' && adminRoutes.includes(req.url))
-            ) {
-                //3.Adding uuid and sessionID to req.body
-                req.body.uuid = jwtdata.uuid
-                req.body.sessionID = jwtdata.sessionID
+            if (req.method === "GET") {
+                req.body.uuid = jwtdata.uuid;
+                req.body.sessionID = jwtdata.sessionID;
                 next();
             }
-            else {
-                res.status(400).json({ data: null, error: "Invalid request URL" });
+            if (
+                (jwtdata.role === "student" &&
+                    studentRoutes.includes(req.url)) ||
+                (jwtdata.role === "teacher" &&
+                    teacherRoutes.includes(req.url)) ||
+                (jwtdata.role === "admin" && adminRoutes.includes(req.url))
+            ) {
+                //3.Adding uuid and sessionID to req.body
+                req.body.uuid = jwtdata.uuid;
+                req.body.sessionID = jwtdata.sessionID;
+                next();
+            } else {
+                res.status(400).json({
+                    data: null,
+                    error: "Invalid request URL",
+                });
                 return;
             }
         }
     } catch (e) {
-        if (typeof e.message === 'undefined') {
-            if (e === 'Session expired') {
+        if (typeof e.message === "undefined") {
+            if (e === "Session expired") {
                 res.clearCookie("login");
             }
             res.status(401).json({ data: null, error: e });
