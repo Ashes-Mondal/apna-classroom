c7a02f5d04ca75a0bfdea0c5d7feb7084841cc0a
subodhr258 <58080782+subodhr258@users.noreply.github.com>
2021-11-03 12:44:22 +0530

    Assignments submissions part2 (#25)
    * Assignments-Submissions-part1
    
    * Assignments submissions part2

--- frontend/src/components/pages/Assignment/SubmissionsList.js
+++ frontend/src/components/pages/Assignment/SubmissionsList.js
@@ -1,13 +1,37 @@
 import React from "react";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 function SubmissionsList({ assignment, theme, setShowSubmissions }) {
+    const submissions = [...Array(100).keys()];
     return (
-        <div className="container">
+        <div className="submissions-list-container">
             <div className="asg-toprow">
                 <h2>{assignment.title}</h2>
                 <span className={`showsubmissions-btn bg-${theme}`} onClick={() => setShowSubmissions(false)}>
                     show assignment
                 </span>
+            </div>
+            <br />
+            <div className="asg-secondrow">
+                <span className="asg-secondrow1">
+                    <h4>Class Submissions</h4>
+                    <span className={`asg-filter-btn bg-${theme}`}>
+                        Filter
+                        <MdKeyboardArrowDown />
+                    </span>
+                </span>
+                <h4>Total Submissions: 69/120</h4>
+            </div>
+            <div className="submissions-list">
+                {submissions.map(() => (
+                    <span className="submission-item">
+                        <span className="submission-item-name">Aaryak Shah </span>
+                        <span className="submission-item-pipeline">|</span>
+                        <span className={`submission-item-rollno font-${theme} bold `}> 2019IMT-001 </span>
+                        <span className="submission-item-marks">__/20</span>
+                        <span className={`submission-item-attachments font-${theme} bold`}>Attachments: 3 </span>
+                    </span>
+                ))}
             </div>
         </div>
     );
